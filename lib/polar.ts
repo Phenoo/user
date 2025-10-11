79871a8416d0c4db7b7dac73229f9379bf5851f4
Phenoo <descometusah@gmail.com>
2025-10-11 16:25:20 +0100

    fix prod 77

--- lib/polar.ts
+++ lib/polar.ts
@@ -1,6 +1,8 @@
 import { Polar } from "@polar-sh/sdk";
 
 export const api = new Polar({
-  server: process.env.NODE_ENV === "development" ? "sandbox" : "production",
-  accessToken: process.env.POLAR_ACCESS_TOKEN,
+  server: "sandbox",
+  // server: process.env.NODE_ENV === "development" ? "sandbox" : "production",
+  accessToken: "polar_oat_sk5VzWwECiW8mlguInjNlXm5CHyp9De8e8x4Y16mYQO",
+  // accessToken: process.env.POLAR_ACCESS_TOKEN,
 });
